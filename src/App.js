80152dc1f01f5b1f6a9663233478177e7914560d
Sizan-Ahmed <asizan631@gmail.com>
2021-10-20 10:27:05 +0600

    few changes done

--- src/App.js
+++ src/App.js
@@ -1,98 +1,59 @@
 // import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-import React, { useEffect, useState } from "react";
 
 function App() {
-  const obj = [
-    { name: "Mobile", price: 10000, brand: "vivo" },
-    { name: "tv", price: 12000, brand: "walton" },
-    { name: "computer", price: 30000, brand: "asus" },
-  ];
-  return (
-    <div className="App">
-      <Phone></Phone>
-      <Users></Users>
-      <header>
-        <ul>
-          {obj.map((fr) => (
-            <div>
-              <li>{fr.name}</li>
-              <li>{fr.price}</li>
-              <li>{fr.brand}</li>
-            </div>
-          ))}
-        </ul>
-        {obj.map((phone) => (
-          <Mobile objs={phone}></Mobile>
-        ))}
-        <Mobile objs={obj[0]}></Mobile>
-        <Mobile objs={obj[1]}></Mobile>
-        <Mobile objs={obj[2]}></Mobile>
-      </header>
-    </div>
-  );
-}
-function Users() {
+  // const product = ["mobile", "laptop", "tv"];
   const [users, setUsers] = useState([]);
+  console.log(users);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((json) => setUsers(json));
-  });
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
+  return (
+    <div className="App">
+      {users.map((us) => {
+        return <User name={us.name} email={us.email}></User>;
+      })}
+      <header></header>
+    </div>
+  );
+}
+
+function User(props) {
   return (
     <div>
-      <h2>dynamic users: {users.length} </h2>
-      <ul>
-        {users.map((loop) => (
-          <li>{loop.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-function Phone() {
-  // const select = document.getElementById("btn");
-  // select.addEventListener("click", function () {
-  //   console.log("clicked");
-  // });
-
-  let [mobile, mobFunc] = useState(10);
-  return (
-    <div>
-      <h1>Quantity: {mobile}</h1>
-      <button
-        onClick={() => {
-          mobFunc(mobile + 1);
-        }}
-      >
-        increase
-      </button>
-      <button
-        onClick={() => {
-          mobFunc(mobile - 1);
-        }}
-      >
-        decrease
-      </button>
+      <h2>name:{props.name}</h2>
+      <h2>email:{props.email}</h2>
     </div>
   );
 }
 
-function Mobile(props) {
-  const { name, price, brand } = props.objs;
-  const a = {
-    border: "2px solid red",
-    width: "400px",
-    margin: "20px",
-  };
-  return (
-    <div style={a}>
-      <p>phone: {name}</p>
-      <p>price: {price}</p>
-      <p>brand: {brand}</p>
-    </div>
-  );
-}
+// function Product(props) {
+//   return <h2>product list:{props.name}</h2>;
+// }
+
+// function First() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <h3>this is first count: {count}</h3>
+//       <Second cnt={count}></Second>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         counter
+//       </button>
+//     </div>
+//   );
+// }
+
+// function Second(props) {
+//   return <h2>this is second count: {props.cnt} </h2>;
+// }
 
 export default App;
