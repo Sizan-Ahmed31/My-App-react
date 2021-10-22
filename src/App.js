e09730d49acba2fde5ba96e6fad968fd2f144e34
Sizan-Ahmed <asizan631@gmail.com>
2021-10-22 11:03:48 +0600

    again change

--- src/App.js
+++ src/App.js
@@ -1,59 +1,38 @@
 // import logo from "./logo.svg";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
 import "./App.css";
 
 function App() {
-  // const product = ["mobile", "laptop", "tv"];
-  const [users, setUsers] = useState([]);
-  console.log(users);
+  return (
+    <div className="App">
+      <header>
+        <Name></Name>
+      </header>
+    </div>
+  );
+}
+
+function Name() {
+  const [user, setName] = useState([]);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((response) => response.json())
+      .then((json) => setName(json));
   }, []);
 
   return (
-    <div className="App">
-      {users.map((us) => {
-        return <User name={us.name} email={us.email}></User>;
-      })}
-      <header></header>
-    </div>
-  );
-}
-
-function User(props) {
-  return (
     <div>
-      <h2>name:{props.name}</h2>
-      <h2>email:{props.email}</h2>
+      <h2>
+        <ul>
+          {user.map((res) => (
+            <li>{res.username}</li>
+          ))}
+        </ul>
+      </h2>
     </div>
   );
 }
-
-// function Product(props) {
-//   return <h2>product list:{props.name}</h2>;
-// }
-
-// function First() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <div>
-//       <h3>this is first count: {count}</h3>
-//       <Second cnt={count}></Second>
-//       <button
-//         onClick={() => {
-//           setCount(count + 1);
-//         }}
-//       >
-//         counter
-//       </button>
-//     </div>
-//   );
-// }
-
-// function Second(props) {
-//   return <h2>this is second count: {props.cnt} </h2>;
-// }
 
 export default App;
